90cc1b52d153857f680a79881fb36aba3b8a36c0
Asif <klmktu.asif@gmail.com>
2020-11-20 05:36:17 +0530

    Stage 20 assignment

---
+++ stage20_assignment/rm.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm ~/expos-roadmap/stage20_assignment/spl_progs/*.xsm
+rm ~/expos-roadmap/stage20_assignment/expl_progs/*.xsm
---
+++ stage20_assignment/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd ~/myexpos/spl/
+./spl ~/expos-roadmap/stage20_assignment/spl_progs/module_boot.spl
+./spl ~/expos-roadmap/stage20_assignment/spl_progs/module_scheduler.spl
+./spl ~/expos-roadmap/stage20_assignment/spl_progs/module_device_manager.spl
+./spl ~/expos-roadmap/stage20_assignment/spl_progs/module_resource_manager.spl
+./spl ~/expos-roadmap/stage20_assignment/spl_progs/module_process_manager.spl
+./spl ~/expos-roadmap/stage20_assignment/spl_progs/module_memory_manager.spl
+./spl ~/expos-roadmap/stage20_assignment/spl_progs/interrupt_console_output.spl
+./spl ~/expos-roadmap/stage20_assignment/spl_progs/interrupt_console_input.spl
+./spl ~/expos-roadmap/stage20_assignment/spl_progs/console_interrupt_handler.spl
+./spl ~/expos-roadmap/stage20_assignment/spl_progs/disk_interrupt_handler.spl
+./spl ~/expos-roadmap/stage20_assignment/spl_progs/exception_handler.spl
+./spl ~/expos-roadmap/stage20_assignment/spl_progs/interrupt_10.spl
+./spl ~/expos-roadmap/stage20_assignment/spl_progs/interrupt_9.spl
+./spl ~/expos-roadmap/stage20_assignment/spl_progs/interrupt_8.spl
+./spl ~/expos-roadmap/stage20_assignment/spl_progs/os_startup.spl
+./spl ~/expos-roadmap/stage20_assignment/spl_progs/sample_timer.spl
+cd ../expl/
+./expl ~/expos-roadmap/stage20_assignment/expl_progs/exec.expl
+./expl ~/expos-roadmap/stage20_assignment/expl_progs/idle.expl
+./expl ~/expos-roadmap/stage20_assignment/expl_progs/odd.expl
+./expl ~/expos-roadmap/stage20_assignment/expl_progs/init.expl
+cd ../xfs-interface/
+./xfs-interface run ~/expos-roadmap/stage20_assignment/batch-xfs
